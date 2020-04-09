9728ae15da11e620ee5597d5679fd5a151f7ea30
Boy Kurniawan <boykurniawan123@gmail.com>
2020-04-09 10:39:29 +0700

    update lisk mk

--- application/views/materi/list_materi.php
+++ application/views/materi/list_materi.php
@@ -8,11 +8,7 @@
 	      	<div class="callout">
 	      		<?php 
 	      		if ($this->session->userdata('level') == '3') {
-	      			$dosen = $this->session->userdata('keterangan');
-	      			$thn = tahun_akademik_aktif();
-	      			$query="SELECT mm.kode_makul,mm.nama_makul,jk.jadwal_id
-                    FROM akademik_jadwal_kuliah as jk,makul_matakuliah as mm
-                    WHERE mm.makul_id=jk.makul_id and jk.dosen_id=$dosen and jk.tahun_akademik_id=$thn";
+	      			$sql = $this->db->get_where('v_krs', array('tahun_akademik_id'=>tahun_akademik_aktif(),'dosen_id'=>$this->session->userdata('keterangan')));
 	      			$sql = $this->db->query($query);
 	      		} elseif ($this->session->userdata('level') == '4') {
 
